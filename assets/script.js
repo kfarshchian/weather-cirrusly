0b9b7bd94871bf40b8d37d9f6c30f2018edb0fac
Kameron <kfarshchian@gmail.com>
2022-11-30 13:27:30 -0700

    Updated css/index/js APIs working. hide and undide divs working

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,53 @@
-// var button = document.querySelector("#button");
-// var inputValue = document.querySelector('#inputValue');
-// var nameEl = document.querySelector('#name');
-// var desc = document.querySelector('#desc');
-// var temp = document.querySelector('#temp');
-// var day = document.querySelector('#day');
-// var wind = document.querySelector('#wind');
-// var humidity = document.querySelector('#humidity');
+var button = document.querySelector("#button");
+var inputValue = document.querySelector('#inputValue');
+var nameEl = document.querySelector('#name');
+var desc = document.querySelector('#currentImg');
+var currentTemp = document.querySelector('#currentTemp');
+var currentDay = document.querySelector('#currentDay');
+var currentWind = document.querySelector('#currentWind');
+var currentHumidity = document.querySelector('#currentHumidity');
+var unhideDiv =  document.getElementById("icons");
+var unhideDiv1 =  document.getElementById("icons1");
+var unhideDiv2 =  document.getElementById("icons2");
+var unhideDiv3 =  document.getElementById("icons3");
+var unhideDiv4 =  document.getElementById("icons4");weatherImg
+var hideDiv =  document.getElementById("weatherImg");
+
+var timeDisplayEl = $('#time-display');
+
+//pulling the time and date from dayjs
+function displayTime() {
+  var rightNow = dayjs().format('M/D/YYYY');
+  timeDisplayEl.text(rightNow);
+}
+
+displayTime()
+
+function getCurrentApi() {
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=06b380e6671f5b35874bb3a59ed6a6df&units=imperial';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      var tempValue = data['main']['temp'];
+      var descImg = "<img src =http://openweathermap.org/img/wn/" + data['weather'][0]['icon'] + ".png>"; 
+      var humidityValue = data['main']['humidity'];
+      var windValue = data['wind']['speed'];
+      
+      currentTemp.innerHTML = "temp: " + tempValue;
+      currentDesc.innerHTML = descImg;
+      currentHumidity.innerHTML = "Humidity: " + humidityValue + " %";
+      currentWind.innerHTML = "Wind: " + windValue + " MPH";
+
+      
+
+    });
+}
+button.addEventListener('click', getCurrentApi);
 
 
 function getApi() {
@@ -17,29 +59,39 @@ function getApi() {
       })
       .then(function (data) {
         console.log(data);
-       for(i = 0; i < 5; i++){
-        document.getElementById("temp" +(i+1)).innerHTML ="temp: " +data['list'][i]['main']['temp'];
-       }
-       
-       
-       
-       
-       
-        // var nameValue = data['city']['name'];
-        // var tempValue = data['list'][0]['main']['temp'];
-        // var descValue = data['list'][0]['weather'][0]['description'];
-        // var dayValue = data['list'][0]['dt_txt']
-        // var humidityValue = data['list'][0]['main']['humidity'];
-        // var windValue = data['list'][0]['wind']['speed'];
-        
-        // nameEl.innerHTML = nameValue;
-        // temp.innerHTML = "temp: " + tempValue;
-        // desc.innerHTML = descValue;
-        // day.innerHTML = "Date: " + dayValue;
-        // humidity.innerHTML = "Humidity: " + humidityValue + " %";
-        // wind.innerHTML = "Wind: " + windValue + " MPH";
+        for(i = 3; i < 30; i+=8){
+          document.getElementById("temp" +(i+1)).innerHTML ="temp: " +data['list'][i]['main']['temp'];
+         }
+         for(i = 3; i < 30; i+=8){
+          document.getElementById("day" +(i+1)).innerHTML ="Date: " +data['list'][i]['dt_txt'].split(' ')[0];
+         }
+         for(i = 3; i < 30; i+=8){
+          document.getElementById("wind" +(i+1)).innerHTML ="wind: " +data['list'][i]['wind']['speed'] + " MPH";
+         }
+         for(i = 3; i < 30; i+=8){
+          document.getElementById("humidity" +(i+1)).innerHTML ="Humidity: " +data['list'][i]['main']['humidity']+ " %";
+         }
+         for(i = 3; i < 30; i+=8){
+          document.getElementById("img" +(i+1)).src ="http://openweathermap.org/img/wn/" + data['list'][i]['weather'][0]['icon']+".png";
+         }
+    
+        var nameValue = data['city']['name'];
+        nameEl.innerHTML = nameValue;
+
+
       });
   }
   button.addEventListener('click', getApi);
 
+  function removeHide(){
+
+    unhideDiv.classList.remove('hide');
+    unhideDiv1.classList.remove('hide');
+    unhideDiv2.classList.remove('hide');
+    unhideDiv3.classList.remove('hide');
+    unhideDiv4.classList.remove('hide');
+    hideDiv.classList.add('hide');
   
+  }
+  
+    button.addEventListener('click', removeHide);
